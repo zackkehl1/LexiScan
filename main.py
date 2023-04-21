d56ad8e62f6ae0eae4fa20985ffff055da6908b7
Zack Kehl <zack.kehl@gmail.com>
2023-04-21 11:56:07 -0400

    Create main.py
    This is a Python script that utilizes the SpeechRecognition library to recognize speech from a microphone input, and the langdetect library to detect the language of the speech transcription. The script begins by checking that the input arguments are appropriate instances of the Recognizer and Microphone classes, and then adjusts the recognizer sensitivity to ambient noise before recording the audio from the microphone.
    
    The script returns a response object that contains a success boolean, an error message (if applicable), and the transcription of the speech. If the transcription is successfully recognized, the script uses langdetect to determine the language of the transcription.
    
    Overall, this script can be useful for tasks that involve processing speech input and identifying the language of the spoken words. For example, it could be used in speech-to-text translation applications or speech analytics tools

---
+++ main.py
@@ -0,0 +1,53 @@
+import speech_recognition as sr
+
+from langdetect import detect, DetectorFactory
+
+def recognize_speech_from_mic(recognizer, microphone):
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    # adjust the recognizer sensitivity to ambient noise and record audio
+    # from the microphone
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+
+recognizer = sr.Recognizer()
+microphone = sr.Microphone()
+
+response = recognize_speech_from_mic(recognizer, microphone)
+
+# use langdetect to detect the language of the transcription
+DetectorFactory.seed = 0
+language = detect(response["transcription"])
+
+# print the original transcription and detected language
+print(f"Input Text: {response['transcription']}")
+print(f"Detected Language: {language}")
